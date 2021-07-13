a5a6c8b4c0c0ab497515d478e25d63c63e2037a1
hadi <hadi_960@hotmail.com>
2021-07-13 15:24:21 -0500

    adds login/username

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,34 @@ require_relative '../config/environment'
 # require_all 'apps'
 
 
-puts "HELLO WORLD"
+prompt = TTY::Prompt.new
+current_user = nil
+
+name_response = prompt.ask('What is your name?')
+
+puts name_response
+
+menu_choice = prompt.select('What would you like to do?', ["Login", "Signup"])
+
+if menu_choice === "Login"
+    user_name = prompt.ask("Email:")
+    password = prompt.mask("Password:")
+
+
+    current_user = User.all.find { |user| user.email === user_name && user.password === password }
+
+    puts "Hello #{current_user.email}!!!!"
+
+#     doctor_choice = prompt.select("Select a doctor below:", Doctor.all)
+#     date_choice = prompt.select("Choose a date", [Date.parse("10/10/2010"), Date.parse("10/11/2010"), Date.parse("10/12/2010")])
+
+#     appt_confirmation = prompt.select("Are you sure you want to book an appt with #{doctor_choice.name} at #{date_choice}?", ["Yes", "No"])
+
+#     if appt_confirmation === "Yes"
+#         Appointment.create(doctor_id: doctor_choice.id, patient_id: current_user.id, date: date_choice)
+
+#         puts "Your appoitment has been booked!"
+#     end
+
+# end
+end
